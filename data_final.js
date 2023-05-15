5e2611353d8dc45f032748cebcff51be0901e9c6
NahuelCuitino <nahuel.cuitino@insite-la.com>
2023-05-15 16:40:42 -0300

    test

---
+++ data_final.js
@@ -0,0 +1,296 @@
+let belgrano = [
+  {
+  "id": 0,
+  "title": "Salas 2d",
+  "type": [
+      {
+        "general": "General de jueves a domingos... <b>$1700</b>",
+        "online": "Online 25% off de jueves a domingos... <b>$1275</b>",
+        "ninos": "Niños hasta 12 años y mayores de 60... <b>$1100</b>",
+        "promo": "Promoción lunes, martes y miércoles... <b>$1100</b>",
+      }
+    ]
+  },{
+  "id": 1,
+  "title": "Salas 2d Superseat",
+  "type": [
+    {
+      "general": "General de jueves a domingos... <b>$1950</b>",
+      "online": "Online 25% off de jueves a domingos... <b>$1462,50</b>",
+      "ninos": "Niños hasta 12 años y mayores de 60... <b>$1250</b>",
+      "promo": "Promoción lunes, martes y miércoles... <b>$1250</b>",
+  }
+    ]
+  },{
+  "id": 2,
+  "title": "Salas 3d",
+  "type": [
+    {
+      "general": "General de jueves a domingos... <b>$2000</b>",
+      "online": "Online 25% off de jueves a domingos... <b>$1500</b>",
+      "ninos": "Niños hasta 12 años y mayores de 60... <b>$1300</b>",
+      "promo": "Promoción lunes, martes y miércoles... <b>$1300</b>",
+  }
+    ]
+  },{
+  "id": 3,
+  "title": "Salas 3d Superseat",
+  "type": [
+    {
+      "general": "General de jueves a domingos... <b>$2250</b>",
+      "online": "Online 25% off de jueves a domingos... <b>$1687,50</b>",
+      "ninos": "Niños hasta 12 años y mayores de 60... <b>$1400</b>",
+      "promo": "Promoción lunes, martes y miércoles... <b>$1400</b>",
+  }
+    ]
+  }
+];
+
+let norcenter = [
+  {
+  "id": 0,
+  "title": "Salas 2d",
+  "type": [
+      {
+        "general": "General de jueves a domingos... <b>$1700</b>",
+        "online": "Online 25% off de jueves a domingos... <b>$1275</b>",
+        "ninos": "Niños hasta 12 años y mayores de 60... <b>$1100</b>",
+        "promo": "Promoción lunes, martes y miércoles... <b>$1100</b>",
+      }
+    ]
+  },{
+  "id": 1,
+  "title": "Salas 2d Superseat",
+  "type": [
+    {
+      "general": "General de jueves a domingos... <b>$1950</b>",
+      "online": "Online 25% off de jueves a domingos... <b>$1462,50</b>",
+      "ninos": "Niños hasta 12 años y mayores de 60... <b>$1250</b>",
+      "promo": "Promoción lunes, martes y miércoles... <b>$1250</b>",
+  }
+    ]
+  },{
+  "id": 2,
+  "title": "Salas 3d",
+  "type": [
+    {
+      "general": "General de jueves a domingos... <b>$2000</b>",
+      "online": "Online 25% off de jueves a domingos... <b>$1500</b>",
+      "ninos": "Niños hasta 12 años y mayores de 60... <b>$1300</b>",
+      "promo": "Promoción lunes, martes y miércoles... <b>$1300</b>", 
+  }
+    ]
+  },{
+  "id": 3,
+  "title": "Salas 3d Superseat",
+  "type": [
+    {
+      "general": "General de jueves a domingos... <b>$2250</b>",
+      "online": "Online 25% off de jueves a domingos... <b>$1687,50</b>",
+      "ninos": "Niños hasta 12 años y mayores de 60... <b>$1400</b>",
+      "promo": "Promoción lunes, martes y miércoles... <b>$1400</b>",
+  }
+    ]
+  }
+];
+
+let haedo = [
+  {
+  "id": 0,
+  "title": "Salas 2d",
+  "type": [
+      {
+        "general": "General de jueves a domingos... <b>$1600</b>",
+        "online": "Online 25% off de jueves a domingos... <b>$1200</b>",
+        "ninos": "Niños hasta 12 años y mayores de 60... <b>$1000</b>",
+        "promo": "Promoción lunes, martes y miércoles... <b>$1000</b>",
+      }
+    ]
+  },{
+  "id": 2,
+  "title": "Salas 3d",
+  "type": [
+    {
+      "general": "General de jueves a domingos... <b>$1800</b>",
+      "online": "Online 25% off de jueves a domingos... <b>$1350</b>",
+      "ninos": "Niños hasta 12 años y mayores de 60... <b>$1150</b>",
+      "promo": "Promoción lunes, martes y miércoles... <b>$1150</b>",
+  }
+    ]
+  }
+];
+
+let quilmes = [
+  {
+  "id": 0,
+  "title": "Salas 2d",
+  "type": [
+      {
+        "general": "General de jueves a domingos... <b>$1700</b>",
+        "online": "Online 25% off de jueves a domingos... <b>$1275</b>",
+        "ninos": "Niños hasta 12 años y mayores de 60... <b>$1100</b>",
+        "promo": "Promoción lunes, martes y miércoles... <b>$1100</b>",
+      }
+    ]
+  },{
+  "id": 1,
+  "title": "Salas 2d Superseat",
+  "type": [
+    {
+      "general": "General de jueves a domingos... <b>$1950</b>",
+      "online": "Online 25% off de jueves a domingos... <b>$1462,50</b>",
+      "ninos": "Niños hasta 12 años y mayores de 60... <b>$1250</b>",
+      "promo": "Promoción lunes, martes y miércoles... <b>$1250</b>",
+  }
+    ]
+  },{
+  "id": 2,
+  "title": "Salas 3d",
+  "type": [
+    {
+      "general": "General de jueves a domingos... <b>$2000</b>",
+      "online": "Online 25% off de jueves a domingos... <b>$1500</b>",
+      "ninos": "Niños hasta 12 años y mayores de 60... <b>$1300</b>",
+      "promo": "Promoción lunes, martes y miércoles... <b>$1300</b>",
+  }
+    ]
+  },{
+  "id": 3,
+  "title": "Salas 3d Superseat",
+  "type": [
+    {
+      "general": "General de jueves a domingos... <b>$2250</b>",
+      "online": "Online 25% off de jueves a domingos... <b>$1687,50</b>",
+      "ninos": "Niños hasta 12 años y mayores de 60... <b>$1400</b>",
+      "promo": "Promoción lunes, martes y miércoles... <b>$1400</b>",
+  }
+    ]
+  }
+];
+
+
+let cordoba = [
+  {
+  "id": 0,
+  "title": "Salas 2d",
+  "type": [
+      {
+        "general": "General de jueves a domingos... <b>$1700</b>",
+        "online": "Online 25% off de jueves a domingos... <b>$1275</b>",
+        "ninos": "Niños hasta 12 años y mayores de 60... <b>$1100</b>",
+        "promo": "Promoción lunes, martes y miércoles... <b>$1100</b>",
+      }
+    ]
+  },{
+  "id": 2,
+  "title": "Salas 3d",
+  "type": [
+    {
+      "general": "General de jueves a domingos... <b>$2000</b>",
+      "online": "Online 25% off de jueves a domingos... <b>$1500</b>",
+      "ninos": "Niños hasta 12 años y mayores de 60... <b>$1300</b>",
+      "promo": "Promoción lunes, martes y miércoles... <b>$1300</b>",
+  }
+    ]
+  }
+];
+
+let allende = [
+  {
+    "id": 0,
+    "title": "Salas 2d",
+    "type": [
+        {
+          "general": "General de jueves a domingos... <b>$1600</b>",
+          "online": "Online 25% off de jueves a domingos... <b>$1200</b>",
+          "ninos": "Niños hasta 12 años y mayores de 60... <b>$1000</b>",
+          "promo": "Promoción lunes, martes y miércoles... <b>$1000</b>",
+        }
+      ]
+    },{
+    "id": 2,
+    "title": "Salas 3d",
+    "type": [
+      {
+        "general": "General de jueves a domingos... <b>$1800</b>",
+        "online": "Online 25% off de jueves a domingos... <b>$1350</b>",
+        "ninos": "Niños hasta 12 años y mayores de 60... <b>$1150</b>",
+        "promo": "Promoción lunes, martes y miércoles... <b>$1150</b>",
+    }
+      ]
+  }
+];
+
+let rosario = [
+  {
+  "id": 0,
+  "title": "Salas 2d",
+  "type": [
+      {
+        "general": "General de jueves a domingos... <b>$1800</b>",
+        "online": "Online 25% off de jueves a domingos... <b>$1350</b>",
+        "ninos": "Niños hasta 12 años y mayores de 60... <b>$1100</b>",
+        "promo": "Promoción lunes, martes y miércoles... <b>$1100</b>",
+      }
+    ]
+  },{
+  "id": 1,
+  "title": "Salas 2d Superseat",
+  "type": [
+    {
+      "general": "General de jueves a domingos... <b>$2000</b>",
+      "online": "Online 25% off de jueves a domingos... <b>$1500</b>",
+      "ninos": "Niños hasta 12 años y mayores de 60... <b>$1200</b>",
+      "promo": "Promoción lunes, martes y miércoles... <b>$1200</b>",
+  }
+    ]
+  },{
+  "id": 2,
+  "title": "Salas 3d",
+  "type": [
+    {
+      "general": "General de jueves a domingos... <b>$2100</b>",
+      "online": "Online 25% off de jueves a domingos... <b>$1575</b>",
+      "ninos": "Niños hasta 12 años y mayores de 60... <b>$1300</b>",
+      "promo": "Promoción lunes, martes y miércoles... <b>$1300</b>",
+  }
+    ]
+  },{
+  "id": 3,
+  "title": "Salas 3d Superseat",
+  "type": [
+    {
+      "general": "General de jueves a domingos... <b>$2300</b>",
+      "online": "Online 25% off de jueves a domingos... <b>$1725</b>",
+      "ninos": "Niños hasta 12 años y mayores de 60... <b>$1450</b>",
+      "promo": "Promoción lunes, martes y miércoles... <b>$1450</b>",
+  }
+    ]
+  }
+];
+
+let ImaxPrices = [
+  {
+  "id": 0,
+  "title": "SALAS 2D y 3D",
+  "type": [
+      {
+        "general": "General... <b>$2400</b>",
+        "online": "",
+        "ninos": "Niños hasta 12 años y mayores de 60... <b>$1700</b>",
+        "promo": "Promoción miércoles... <b>$1700</b>",
+      }
+    ]
+  },{
+  "id": 1,
+  "title": "PROMOCIÓN 2x1 con APP <b>MÁS SHOWCASE</b>",
+  "type": [
+      {
+        "general": "Válido todos los días... <b>$1200</b> por entrada.",
+        "online": "",
+        "ninos": "",
+        "promo": "",
+      }
+    ]
+  }
+];
